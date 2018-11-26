92d805a6066098f1368bfb205f6218d224da7c34
Felype <felype.oliveira@etec.sp.gov.br>
2018-11-26 20:29:10 -0200

    Foi adicionado uma borda a nota, também os incones dos botões dentro da nota. O JavaScript foi inicializado.

---
+++ index.js
@@ -0,0 +1,9 @@
+btnAdd.addEventListener("click", function(event) {
+	event.preventDefault();
+});
+
+function criarNota(texto, cor){
+	var btnCor
+	var cor = document.getElementById("btnCor").value;
+	console.log(btnCor.value);
+}
