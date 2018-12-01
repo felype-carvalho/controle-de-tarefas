fb269ac2e1f6d74c3dd64331d319b89c07b39df0
fabricioeliasbr <fabricio.silva226@etec.sp.gov.br>
2018-11-30 22:22:01 -0200

    Tentamos mexer no JavaScript.

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-btnAdd.addEventListener("click", function(event) {
+var cor = document.getElementById("btnCor").value;
+inputTexto.textContent = inputTexto;
+var texto = document.getElementById("inputTexto");
+//console.log(cor)
+btnAdd.addEventListener("click", function criarNota(event) {
 	event.preventDefault();
+	/*console.log(cor);
+	console.log(texto.value);
+	*/
 });
 
 function criarNota(texto, cor){
-	var btnCor
-	var cor = document.getElementById("btnCor").value;
-	console.log(btnCor.value);
+	console.log("iae");
+//var notas = document.createElement();
+/*
+create.Element(div maior);
+
+	create.Element(texto.div);
+		create.Element(texto digitado);
+
+	create.Element(icones.div);
+
+		create.Element(icone.div);
+			create.Element(icone);
+		create.Element(icone.div);
+			create.Element(icone);
+		create.Element(icone.div);
+			create.Element(icone);
+*/	
+	divNotas.classList.add("divNotas");
 }
