73a91b3a64abbedff4eadde176e7d6313c3ed1ed
Felype <felype.oliveira@etec.sp.gov.br>
2018-12-02 07:57:50 -0200

    Função Criar Nota criada com sucesso!

--- index.js
+++ index.js
@@ -1,43 +1,71 @@
-var cor = document.getElementById("btnCor").value;
+var cor = document.getElementById("btnCor");
 inputTexto.textContent = inputTexto;
 var texto = document.getElementById("inputTexto");
-//console.log(cor)
+
+function criarNota(){
+	console.log(cor.value);
+	console.log(texto.value);
+
+	//// Criação dos elementos da nota.////
+
+	/* Div Principal */
+	var nota = document.createElement("div");
+
+	/* Div de Texto*/
+	var divTexto = document.createElement("div");
+	var paragrafo = document.createElement("p");
+	var exTexto = document.createTextNode(texto.value);
+
+	/* Div de Botões */
+	var divBotoes = document.createElement("div");
+	var btn1 = document.createElement("button");	
+	var btn2 = document.createElement("button");
+	var btn3 = document.createElement("button");
+	var incone1 = document.createElement("i");
+	var incone2 = document.createElement("i");
+	var incone3 = document.createElement("i");
+
+	//// Hierarquia dos Elementos ////
+
+	/* Div Principal */
+	document.getElementById("notas").appendChild(nota);
+	nota.appendChild(divTexto);
+	nota.appendChild(divBotoes);
+
+	/* Div de Texto*/
+	divTexto.appendChild(paragrafo);
+	paragrafo.appendChild(exTexto);
+
+	/* Div de Botões */
+	divBotoes.appendChild(btn1); 	
+	divBotoes.appendChild(btn2); 
+	divBotoes.appendChild(btn3);
+	btn1.appendChild(incone1);
+	btn2.appendChild(incone2);
+	btn3.appendChild(incone3);
+
+	//// Colocando Classes nos Elementos ////
+
+	/* Div Principal */
+	nota.classList.add("nota");
+	nota.style.backgroundColor = cor.value;
+
+	/* Div de Texto*/
+	divTexto.classList.add("divTexto");
+
+	/* Div de Botões */
+	divBotoes.classList.add("divBotoes");
+	btn1.classList.add("btn");
+	btn2.classList.add("btn");	
+	btn3.classList.add("btn");
+	incone1.classList.add("fa-trash-alt", "fas");
+	incone2.classList.add("fa-edit", "fas");
+	incone3.classList.add("fa-check", "fas");	
+}
+
 btnAdd.addEventListener("click", function(event) {
 	event.preventDefault();
 
 	criarNota();
-	/*console.log(cor);
-	console.log(texto.value);
-	*/
+
 });
-
-function criarNota(texto, cor){
-	console.log("iae");
-
-	var nota = document.createElement("div");
-	var divNota = document.createElement("div");
-	var exTexto = document.createTextNode("p");
-
-	nota.appendChild(divNota);
-	divNota.appendChild(exTexto);
-	document.getElementById("notas").appendChild(nota);
-
-	nota.classList.add("nota");
-
-//var notas = document.createElement();
-/*
-create.Element(div maior);
-
-	create.Element(texto.div);
-		create.Element(texto digitado);
-
-	create.Element(icones.div);
-
-		create.Element(icone.div);
-			create.Element(icone);
-		create.Element(icone.div);
-			create.Element(icone);
-		create.Element(icone.div);
-			create.Element(icone);
-*/	
-}
