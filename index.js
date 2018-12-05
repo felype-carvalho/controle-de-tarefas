3a1ef8ae79b0addf6e31d4cae00c67fb3b94dc8d
Felype <felype.oliveira@etec.sp.gov.br>
2018-12-05 06:30:11 -0200

    Função editarNota terminada.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var cor = document.getElementById("btnCor");
-inputTexto.textContent = inputTexto;
 var texto = document.getElementById("inputTexto");
-var btnApagar = document.getElementById("btnApagar");
-var notas = document.querySelector("#notas");
+var form = document.querySelector("#form-notas");
+var btnSalvar = criarBotaoSalvar("fa-check");
 
 function criarNota(){
 
@@ -14,14 +13,15 @@ function criarNota(){
 	/* Div de Texto*/
 	var divTexto = document.createElement("div");
 	var paragrafo = document.createElement("p");
-	var exTexto = document.createTextNode(texto.value);
+	paragrafo.textContent = texto.value;
+	//var exTexto = document.createTextNode(texto.value);
 
 	/* Div de Botões */
 	var divBotoes = document.createElement("div");
 	var btn1 = document.createElement("button");	
 	var btn2 = document.createElement("button");
-	var incone1 = document.createElement("i");
-	var incone2 = document.createElement("i");
+	var icone1 = document.createElement("i");
+	var icone2 = document.createElement("i");
 
 	//// Hierarquia dos Elementos ////
 
@@ -32,13 +32,13 @@ function criarNota(){
 
 	/* Div de Texto*/
 	divTexto.appendChild(paragrafo);
-	paragrafo.appendChild(exTexto);
+	//paragrafo.appendChild(exTexto);
 
 	/* Div de Botões */
 	divBotoes.appendChild(btn1); 	
 	divBotoes.appendChild(btn2); 
-	btn1.appendChild(incone1);
-	btn2.appendChild(incone2);
+	btn1.appendChild(icone1);
+	btn2.appendChild(icone2);
 
 	//// Colocando Classes nos Elementos ////
 
@@ -48,19 +48,26 @@ function criarNota(){
 
 	/* Div de Texto*/
 	divTexto.classList.add("divTexto");
+	paragrafo.classList.add("texto");
 
 	/* Div de Botões */
 	divBotoes.classList.add("divBotoes");
 	btn1.classList.add("btn", "btn-apagar");
 	btn2.classList.add("btn");	
-	incone1.classList.add("fa-trash-alt", "fas");
-	incone2.classList.add("fa-edit", "fas");
+	icone1.classList.add("fa-trash-alt", "fas");
+	icone2.classList.add("fa-edit", "fas");
 
 	// Escutadores de Eventos //
 	btn1.addEventListener("click", apagarNota);
 	btn2.addEventListener("click", editarNota);	
 }
 
+btnAdd.addEventListener("click", function(event) {
+	event.preventDefault();
+
+	criarNota();
+
+});
 
 function apagarNota (event) {
 		var nota;
@@ -82,42 +89,43 @@ btnApagar.forEach(function(botao){
 	botao.addEventListener("click", apagarNota);
 });
 
-function criarBotão (event) {
-	var inconeSalvar = document.createElement("i");
+function criarBotaoSalvar (icone) {
+	var iconeSalvar = document.createElement("i");
 	var btnSalvar = document.createElement("button");
-	btnSalvar.appendChild(inconeSalvar);
-	btnSalvar.classList.add("btn");
-	inconeSalvar.classList.add("fas", "fa-check");
+	btnSalvar.appendChild(iconeSalvar);
+	btnSalvar.classList.add("btnSalvar");
+	iconeSalvar.classList.add("fas", icone);
 	return btnSalvar;
 }
-
-function editarNota (event) {
-	console.log("PEEEEI");
-	/*if(event.target.hasChildNodes()) {
-		linha = event.target.parentNode.parentNode;
-	} else {
-		linha = event.target.parentNode.parentNode.parentNode;
-	}
-	var nome = linha.querySelector(".nome").textContent;
-	var qtde = linha.querySelector(".qtde").textContent;
-	var preco = linha.querySelector(".preco").textContent;
-
-	form.nome.value = nome;
-	form.qtde.value = qtde;
-	form.preco.value = preco;
-
-	btnSalvar.classList.add('invisivel');
-	form.appendChild(btnConfirmar);*/
-};
 
 var btnEditar = document.querySelectorAll(".btn-editar");
 btnEditar.forEach(function(botao){
 	botao.addEventListener("click", editarNota);
 });
 
-btnAdd.addEventListener("click", function(event) {
+function editarNota (event) {
+	//console.log("PEEEEI");
+	if(event.target.hasChildNodes()) {
+		nota = event.target.parentNode.parentNode;
+	} else {
+		nota = event.target.parentNode.parentNode.parentNode;
+	}
+	var texto = nota.querySelector(".texto").textContent;
+
+	console.log(texto);
+	inputTexto.value = texto;
+
+	btnAdd.classList.add('invisivel');
+	form.appendChild(btnSalvar);
+};
+
+btnSalvar.addEventListener("click", function(event){
 	event.preventDefault();
 
-	criarNota();
-
+	nota.querySelector(".texto").textContent = inputTexto.value;
+	
+	btnSalvar.remove();
+	btnAdd.classList.remove("invisivel");
+	form.reset();
 });
+
